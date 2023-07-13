cc23172f23f7242cfe89405b9f8577a494017fb6
viktorevstigneev <viktor_evstignnev14@bk.ru>
2023-07-13 05:17:30 +0300

    feature/hologramma: add node version

---
+++ src/components/common/Slider/Slider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+const Slider = () => {
+	return (
+		<div className="slider">
+			<div className="siler__item"></div>
+			<div className="siler__item"></div>
+			<div className="siler__item"></div>
+		</div>
+	);
+};
+
+export default Slider;
--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,12 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 
-import './style.css';
+import { Main } from './styles.js';
+// import Slider from '../../common/Slider/index.js';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics, usePlane, useBox } from '@react-three/cannon';
+
+function Box() {
+	const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+	return (
+		<mesh
+			onClick={() => {
+				api.velocity.set(0, 2, 0);
+			}}
+			ref={ref}
+			position={[0, 2, 0]}
+		>
+			<boxGeometry attach="geometry" />
+			<meshLambertMaterial attach="material" color="hotpink" />
+		</mesh>
+	);
+}
+
+function Plane() {
+	const [ref] = usePlane(() => ({
+		rotation: [-Math.PI / 2, 0, 0],
+	}));
+	return (
+		<mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+			<planeGeometry attach="geometry" args={[100, 100]} />
+		</mesh>
+	);
+}
 
 const MainPage = () => {
 	return (
-		<>
-			<main className="home__main">fghb</main>
-		</>
+		<Main>
+			<Canvas>
+				<Suspense fallback="loading">
+				<OrbitControls />
+				<Stars />
+				<ambientLight intensity={0.5} />
+				<spotLight position={[10, 15, 10]} angle={0.3} />
+				<Physics>
+					<Box />
+					<Plane />
+				</Physics>
+				</Suspense>
+			</Canvas>
+		</Main>
 	);
 };
 
