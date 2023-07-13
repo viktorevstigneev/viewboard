0de50cf73024e1026a6e5899485225cd66d0e73d
viktorevstigneev <viktor_evstignnev14@bk.ru>
2023-07-13 07:17:27 +0300

    feature/hologramma: ad car

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,68 +1,86 @@
-import React, { Suspense, useEffect, useLayoutEffect, useRef, useState } from 'react';
-
+import React, { Suspense } from 'react';
 import { Main } from './styles.js';
-import * as THREE from 'three'
-// import Slider from '../../common/Slider/index.js';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stars } from '@react-three/drei';
-import { Physics, usePlane, useBox } from '@react-three/cannon';
+import { EffectComposer, DepthOfField, Bloom, ChromaticAberration } from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
+import { CubeCamera, Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Boxes } from '../../common/Boxes/Boxes';
+import { Car } from '../../common/Car/Car';
+import { Ground } from '../../common/Ground/Ground';
+import { FloatingGrid } from '../../common/FloatingGrid/FloatingGrid';
+import { Rings } from '../../common/Rings/Rings';
 
-function Box() {
-	const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+function CarShow() {
 	return (
-		<mesh
-			onClick={() => {
-				api.velocity.set(0, 2, 0);
-			}}
-			ref={ref}
-			position={[0, 2, 0]}
-		>
-			<boxGeometry attach="geometry" />
-			<meshLambertMaterial attach="material" color="hotpink" />
-		</mesh>
+		<>
+			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+
+			<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+			<color args={[0, 0, 0]} attach="background" />
+
+			<CubeCamera resolution={256} frames={Infinity}>
+				{(texture) => (
+					<>
+						<Environment map={texture} />
+						<Car />
+					</>
+				)}
+			</CubeCamera>
+
+			<spotLight
+				color={[1, 0.25, 0.7]}
+				intensity={1.5}
+				angle={0.6}
+				penumbra={0.5}
+				position={[5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+			<spotLight
+				color={[0.14, 0.5, 1]}
+				intensity={2}
+				angle={0.6}
+				penumbra={0.5}
+				position={[-5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+			<Ground />
+			<FloatingGrid />
+			<Boxes />
+			<Rings />
+
+			<EffectComposer>
+				{/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+				<Bloom
+					blendFunction={BlendFunction.ADD}
+					intensity={1.3} // The bloom intensity.
+					width={300} // render width
+					height={300} // render height
+					kernelSize={5} // blur kernel size
+					luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+					luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+				/>
+				<ChromaticAberration
+					blendFunction={BlendFunction.NORMAL} // blend mode
+					offset={[0.0005, 0.0012]} // color offset
+				/>
+			</EffectComposer>
+		</>
 	);
 }
 
-function Plane() {
-	const [ref] = usePlane(() => ({
-		rotation: [-Math.PI / 2, 0, 0],
-	}));
-	return (
-		<mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-			<planeGeometry attach="geometry" args={[100, 100]} />
-		</mesh>
-	);
-}
 
-function Line({ start, end }) {
-	const ref = useRef();
-	useLayoutEffect(() => {
-		ref.current.geometry.setFromPoints([start, end].map((point) => new THREE.Vector3(...point)));
-	}, [start, end]);
-	return (
-		<line ref={ref} >
-			<bufferGeometry />
-			<lineBasicMaterial  color="aqua" />
-		</line>
-	);
-}
 
 const MainPage = () => {
 	return (
 		<Main>
-			<Canvas>
-				<Suspense fallback="loading">
-					<OrbitControls />
-					<Stars />
-					<ambientLight intensity={0.5} />
-					<spotLight position={[10, 15, 10]} angle={0.3} />
-					<Physics>
-						<Box />
-						<Line start={[0, 0, 1]} end={[1, 1, 0]} />
-						<Plane />
-					</Physics>
-				</Suspense>
-			</Canvas>
+			<Suspense fallback={null}>
+				<Canvas shadows>
+					<CarShow />
+				</Canvas>
+			</Suspense>
 		</Main>
 	);
 };
